a162416c1f94913982e576617723cae8dc93e9dd
MarkusPeitl <e1526724@student.tuwien.ac.at>
2019-07-30 16:43:15 +0200

    Added gravity based sinepath

--- imgsmoke.py
+++ imgsmoke.py
@@ -259,49 +259,92 @@ def createSinePath(startx,starty,rows,cols):
 
     return linepath
 
-def createSinePath2D(startx,starty,rows,cols):
-    height = 100
-    nwaves = 20
-    pathdist = 200
-    anglerad = math.pi/4
-    xdir = math.cos(anglerad)
-    ydir = math.sin(anglerad)
+def magnitude(pos1,pos2):
+    return math.sqrt(math.pow(pos2[0] - pos1[0],2) + math.pow(pos2[1] - pos1[1],2))
+
+def drawPhysicShot(img):
+    imgShape = img.shape
+
+    rows, cols, ch = imgShape
+    output = np.zeros((rows,cols),np.uint8)
+
+    seedPoint = np.array([int(cols/2),int(rows/2)])
+    seedPoint = seedPoint.astype(np.int32)
+
+    forceDir = np.array([0.1,-1])
+    shotForce = 85
+
+    gravityDir = np.array([0,1])
+    #gravityConstant = 9.81
+    gravityConstant = 4
+
+    distance = 0
+
+    distanceDiff = 0.01
+    #distanceDiff = 0.5
+    pos = seedPoint
+    while(pos[0] >= 0 and pos[0] < cols and pos[1] >= 0 and pos[1] < rows):
+        #print(pos)
+        output[pos[1],pos[0]] = 255
+        #distance = math.sqrt(math.pow(pos[0] - seedPoint[0],2) + math.pow(pos[1] - seedPoint[1],2))
+        distance += distanceDiff
+        #print("distance: " + str(distance))
+        forceDiffComponent = forceDir * shotForce
+        gravityDiffComponent = gravityDir * gravityConstant * distance
+
+        #particle velocity in current point
+        velocityComponent = forceDiffComponent + gravityDiffComponent
+
+        velocityMagnitude = math.sqrt(math.pow(velocityComponent[0],2) + math.pow(velocityComponent[1],2))
+        distanceDiff = 1/(velocityMagnitude)
+        print(distanceDiff)
+
+        posChangeComponent = velocityComponent * distance
+        pos = (seedPoint + posChangeComponent).astype(np.int32)
+        #print(pos)
+
+    cv2.imshow("drawPhysicShot",output)
+
+def createSinePath2D(startx,starty,rows,cols,axisangle):
+    startheight = 200
+    endheight = 50
+    nwaves = 2
+    pathdist = 500
+    #anglerad = math.pi/3
+    anglerad = axisangle
+    samplescalef = 1
+    xdir = math.cos(anglerad)/samplescalef
+    ydir = math.sin(anglerad)/samplescalef
+    normxdir = -ydir
+    normydir = xdir
     print('xdir: ' + str(xdir) + " /ydir: " + str(ydir))
-    #direction = [0,1]
-    direction = (xdir,ydir)
+    startheight = startheight * samplescalef
+    endheight = endheight * samplescalef
+    heightdiff = endheight - startheight
+    #direction = (xdir,ydir)
 
-    #if(xlength > cols - startx):
-    #    xlength = cols - startx
-    
     linepath = []
-    
-    #stepy = 2 * math.pi * 1/cols * nwavesy
-    
-    if abs(xdir) > abs(ydir):
-        step = 2 * math.pi * 1/cols * nwaves
-        start = startx
-        end = int(start + xdir * pathdist)
-    else:
-        step = 2 * math.pi * 1/cols * nwaves
-        start = starty
-        end = int(start + ydir * pathdist)
-
-    print("step: " + str(step) + " /start: " + str(start) + " /end: " + str(end))
+ 
+    #print("step: " + str(step) + " /start: " + str(start) + " /end: " + str(end))
 
     currentx = startx
     currenty = starty
     currentdist = 0
+    startphase = math.pi * 1
 
     outofbounds = False
     cnt = 0
+    sinanglestep = 2 * math.pi * nwaves
     while currentdist < pathdist and not outofbounds:
+        percentxy = currentdist/pathdist
         currentx = startx + cnt * xdir
         currenty = starty + cnt * ydir
         currentdist = math.sqrt(math.pow(currentx - startx,2) + math.pow(currenty - starty,2))
         #print(currentdist)
-        x = currentx + int(math.sin(cnt/10) * (height/2) * ydir)
-        #y = currenty + int(math.sin(cnt)*height/2) * ydir
-        y = currenty + int(math.sin(cnt/10) * (height/2) * xdir)
+        currheight = startheight + heightdiff * percentxy
+        waveheight = math.sin(startphase + percentxy * sinanglestep) * (currheight/2)
+        x = currentx + int(waveheight * normxdir)
+        y = currenty + int(waveheight * normydir)
         if(currentx >= 0 and currentx < cols and currenty >= 0 and currenty < rows):
             linepath.append([int(x),int(y)])
         else:
@@ -309,27 +352,17 @@ def createSinePath2D(startx,starty,rows,cols):
 
         cnt += 1
 
-    """for axisit in range(start,end):
-        #othermagnitude = int(start + math.sin((axisit-start) * step) * height/2)#  +  direction[1] * x
-        #y = starty + int((math.sin((y-starty)*stepy))*(height)/2)
-        if abs(xdir) > abs(ydir):
-            othermagnitude = int(axisit * xdir)
-            linepath.append([axisit,othermagnitude])
-        else:
-            othermagnitude = int(axisit * ydir)
-            linepath.append([othermagnitude,axisit])
-    """
-    print(linepath)
+    #print(linepath)
     return linepath
 
 
-def drawPathMask(rows,cols,linepath):
+def drawPathMask(rows,cols,linepath,windowname):
     output = np.zeros((rows,cols),np.uint8)
 
     for point in linepath:
         output[point[1],point[0]] = 255
 
-    cv2.imshow('pathmaskln',output)
+    cv2.imshow(windowname,output)
 
 
 def createCirclularMask(ksize,cthickness=10000):
@@ -350,15 +383,24 @@ def createCirclularMask(ksize,cthickness=10000):
         return mask
     return None
 
-def walkPath(img,linepath):
+"""def createRandGeometry2D(seedPoint,imgShape):
+    output = np.zeros((rows,cols),np.uint8)
+"""
+
+
+def walkPaths(img,orgimg,linepaths):
     rows, cols, ch = img.shape
     output = np.zeros((rows,cols),np.uint8)
+    outputmask = np.zeros((rows,cols),np.uint8)
+    outimg = np.zeros((rows,cols,3),np.uint8)
+    img = img.copy()
 
     sigma = 30
-    ksizestart = 200
-    ksizeend = 40
-    startopacity = 0.01
+    ksizestart = 150
+    ksizeend = 1
+    startopacity = 0.08
     endopacity = 0.0
+    filterblackthres = 150
     opacitydiff = startopacity - endopacity
     ksizediff = ksizestart - ksizeend
 
@@ -366,63 +408,70 @@ def walkPath(img,linepath):
     orgGaussK = None
     orgsampleset = False
 
-    lastksize = -1
-    pathlenx = len(linepath)
-    for x in range(0,pathlenx):
-        percentx = x/pathlenx
-        invpercentx = 1 - percentx
-        point = linepath[x]
-        currentksize = int(ksizeend + ksizediff * invpercentx)
+    for linepath in linepaths:
+        lastksize = -1
+        pathlenx = len(linepath)
+        for x in range(0,pathlenx):
+            percentx = x/pathlenx
+            invpercentx = 1 - percentx
+            point = linepath[x]
+            currentksize = int(ksizeend + ksizediff * invpercentx)
 
-        startx = int(point[1] - currentksize/2)
-        endx = int(point[1] + currentksize/2)
-        starty = int(point[0] - currentksize/2)
-        endy = int(point[0] + currentksize/2)
+            startx = int(point[1] - currentksize/2)
+            endx = int(point[1] + currentksize/2)
+            starty = int(point[0] - currentksize/2)
+            endy = int(point[0] + currentksize/2)
 
-        if(currentksize != lastksize):
-            
-            if(not orgsampleset):
-                orgsample = img[startx:endx,starty:endy,:]
-                cv2.imshow('orgsample',orgsample)
-                orgGaussSingleX = cv2.getGaussianKernel(currentksize,sigma) * (sigma/0.4)
-                orgGaussSingleY = cv2.getGaussianKernel(currentksize,sigma) * (sigma/0.4)
-                orgGaussSingle = np.sqrt(orgGaussSingleX * orgGaussSingleY.transpose())
-                #print(orgGaussSingleX)
-                orgGaussK = np.zeros((currentksize,currentksize,3),np.float32)
-                orgGaussK[:,:,0] = orgGaussSingle
-                orgGaussK[:,:,1] = orgGaussSingle
-                orgGaussK[:,:,2] = orgGaussSingle
-                #print(orgGaussSingle)
-                orgsampleset = True
+            if(currentksize != lastksize):
+                
+                if(not orgsampleset):
+                    orgsample = orgimg[startx:endx,starty:endy,:]
+                    cv2.imshow('orgsample',orgsample)
+                    orgGaussSingleX = cv2.getGaussianKernel(currentksize,sigma) * (sigma/0.4)
+                    orgGaussSingleY = cv2.getGaussianKernel(currentksize,sigma) * (sigma/0.4)
+                    orgGaussSingle = np.sqrt(orgGaussSingleX * orgGaussSingleY.transpose())
+                    #print(orgGaussSingleX)
+                    orgGaussK = np.zeros((currentksize,currentksize,3),np.float32)
+                    orgGaussK[:,:,0] = orgGaussSingle
+                    orgGaussK[:,:,1] = orgGaussSingle
+                    orgGaussK[:,:,2] = orgGaussSingle
+                    #print(orgGaussSingle)
+                    orgsampleset = True
 
-            currentsample = cv2.resize(orgsample,(currentksize,currentksize))
-            currentgaussK = cv2.resize(orgGaussK,(currentksize,currentksize))
-            #print(currentgaussK)
-            #img[startx:endx,starty:endy] = (img[startx:endx,starty:endy] + currentsample)/2
+                currentsample = cv2.resize(orgsample,(currentksize,currentksize))
+                currentgaussK = cv2.resize(orgGaussK,(currentksize,currentksize))
+                #print(currentgaussK)
+                #img[startx:endx,starty:endy] = (img[startx:endx,starty:endy] + currentsample)/2
 
-            kernel = np.ones((currentksize,currentksize),np.uint8) * 255
+                lastksize = currentksize
 
-            lastksize = currentksize
+            curropacity = endopacity + opacitydiff * invpercentx
+            invopacity = 1 - curropacity
 
-        curropacity = endopacity + opacitydiff * invpercentx
-        invopacity = 1 - curropacity
+            curropacityK = curropacity * currentgaussK
+            invopacityK = 1 - curropacityK
+            #curropacityK = currentgaussK
+            #invopacityK = 1 - curropacityK
+            if(startx >= 0 and endx < rows and starty >= 0 and endy < cols):
+                if(filterblackthres is not 0):
+                    currentgraysample = cv2.cvtColor(currentsample,cv2.COLOR_BGR2GRAY)
+                    curropacityK[currentgraysample < filterblackthres] = 0.0
+                    invopacityK[currentgraysample < filterblackthres] = 1.0
+                img[startx:endx,starty:endy] = img[startx:endx,starty:endy] * invopacityK + currentsample * curropacityK
+                #outimg[startx:endx,starty:endy] = img[startx:endx,starty:endy] * invopacityK + currentsample * curropacityK
+                output[startx:endx,starty:endy] = currentgaussK[:,:,0] * 255
+                outputmask[startx:endx,starty:endy] = 255
+            else:
+                print(rows)
 
-        curropacityK = curropacity * currentgaussK
-        invopacityK = 1 - curropacityK
-        #curropacityK = currentgaussK
-        #invopacityK = 1 - curropacityK
-        if(startx >= 0 and endx < rows and starty >= 0 and endy < cols):
-            img[startx:endx,starty:endy] = img[startx:endx,starty:endy] * invopacityK + currentsample * curropacityK
-            output[startx:endx,starty:endy] = kernel
-        else:
-            print(rows)
 
-
-
+    #outimg = cv2.bitwise_and(img,img,mask=outputmask)
     #for point in linepath:
     #    output[point[1],point[0]] = 255
-    cv2.imshow('combimg',img)
-    cv2.imshow('pathmask',output)
+    #cv2.imshow('outimg',outimg)
+    #cv2.imshow('combimg',img)
+    #cv2.imshow('pathmask',output)
+    return img
 
 """orgimg = orgimg.astype(np.float32)
 
@@ -441,12 +490,28 @@ cv2.imshow('newimg',orgimg)
 h, w, ch = orgimg.shape
 print('w:' + str(w))
 print('h:' + str(h))
-#sinepath = createSinePath(200,400,h,w)
-sinepath2d = createSinePath2D(400,300,h,w)
+sinepath = createSinePath(500,520,h,w)
+sinepath2d = createSinePath2D(500,520,h,w,math.pi/3)
 #sinepath = createSinePath(530,120,h,w)
-drawPathMask(h,w,sinepath2d)
-#drawPathMask(h,w,sinepath)
-#walkPath(orgimg,sinepath)
+drawPathMask(h,w,sinepath2d,'sine2d')
+drawPathMask(h,w,sinepath,'sine')
+#img1 = walkPath(orgimg,orgimg,sinepath2d)
+#img1 = walkPath(img1,orgimg,createSinePath2D(500,520,h,w,0))
+img1 = orgimg
+linepaths = []
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * -40/360))
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * -20/360))
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * 0/360))
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * 20/360))
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * 40/360))
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * 60/360))
+linepaths.append(createSinePath2D(500,520,h,w,math.pi * 2 * 80/360))
+#img1 = walkPaths(img1,orgimg,linepaths)
+drawPhysicShot(img1)
+
+#img1 = walkPath(img1,orgimg,createSinePath2D(500,520,h,w,math.pi * 2 * -10/360))
+cv2.imshow('testorgimgchanged',orgimg)
+cv2.imshow('combinedwalked',img1)
 #cv2.imshow('bleed',multiwarp(orgimg,[10,20,30,40,50,60,70,80,90,100],2))
 #cv2.imshow('customwarp',customwarp(orgimg))
 
